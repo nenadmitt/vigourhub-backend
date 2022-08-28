6bbbdf2582369462389325a09301de638c5c353b
Nenad Mitkovic <nenadmit@outlook.com>
2022-08-28 15:58:38 +0200

    code skeleton

---
+++ src/main/java/com/vigourhub/backend/VigourhubBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.vigourhub.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VigourhubBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VigourhubBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vigourhub/backend/VigourhubBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vigourhub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VigourhubBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
